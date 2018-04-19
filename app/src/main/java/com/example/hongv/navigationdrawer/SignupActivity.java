92b5a03486c5cab7dd9e4b6d2463a9d14b5556b8
vanle57 <33739005+vanle57@users.noreply.github.com>
2018-04-19 19:54:23 +0700

    Dao them string voi dimen vo nghe!

---
+++ app/src/main/java/com/example/hongv/navigationdrawer/SignupActivity.java
@@ -0,0 +1,104 @@
+package com.example.hongv.navigationdrawer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+public class SignupActivity extends AppCompatActivity {
+
+    ProfilePictureView profilePictureView;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    String name, email, firstname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        Anhxa();
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        setLogin_Button();
+    }
+
+    private void setLogin_Button() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                loginButton.setVisibility(View.INVISIBLE);
+                startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                finish();
+                result();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    private void result() {
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Log.d("JSON",response.getJSONObject().toString());
+                try {
+                    name = object.getString("name");
+                    firstname = object.getString("first_name");
+                    profilePictureView.setProfileId(Profile.getCurrentProfile().getId());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","name,email,first_name");
+        graphRequest.setParameters(parameters);
+        graphRequest.executeAsync();
+    }
+
+    public void Anhxa(){
+        profilePictureView = (ProfilePictureView)findViewById(R.id.imageProfilePictureview);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        LoginManager.getInstance().logOut();
+        super.onStart();
+    }
+}
